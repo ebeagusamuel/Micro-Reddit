11d07316b01aec3f2b1e957627e40afdb00713fa
teekaytech <taofeek.olalere@feghas.com>
2020-06-19 14:15:03 +0100

    Add posts model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :name, presence: true
   validates :username, presence: true, length: { minimum: 6 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
