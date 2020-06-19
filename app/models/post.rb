fdce785fbb1b6baab1383dd39d1aa4b3df75a750
teekaytech <taofeek.olalere@feghas.com>
2020-06-19 14:33:17 +0100

    Add Comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { minimum: 10 }
   validates :body, presence: true, length: { minimum: 10 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, :comments
   validates :name, presence: true
   validates :username, presence: true, length: { minimum: 6 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
